0c2206b67d2a1f005dccdef53d1b67901df7278d
dellacross <victordella652@gmail.com>
2024-07-25 14:45:20 -0300

    add componentes form novo gasto

--- contacerta/frontend/src/components/Expenses/Form/index.jsx
+++ contacerta/frontend/src/components/Expenses/Form/index.jsx
@@ -3,11 +3,40 @@ import './styles.css'
 
 const Form = () => {
 
-    
+    const [currentStep, setCurrentStep] = useState(1)
+
+    const handleSteps = (id) => {
+        if((currentStep+id) >= 1 && (currentStep+id) <= 6) setCurrentStep(currentStep+id)
+    }
 
     return (
         <div id="form-wrapper">
-
+            <div id="container">
+                <aside>
+                    <span>1</span>
+                    <span>2</span>
+                    <span>3</span>
+                    <span>4</span>
+                    <span>5</span>
+                    <span>6</span>
+                </aside>
+                <main>
+                    <header>
+                        <span className={`step ${currentStep === 1 ? 'active' : ''}`}>Categoria</span>
+                        <span className={`step ${currentStep === 2 ? 'active' : ''}`}>Data</span>
+                        <span className={`step ${currentStep === 3 ? 'active' : ''}`}>Descrição</span>
+                        <span className={`step ${currentStep === 4 ? 'active' : ''}`}>Valor</span>
+                        <span className={`step ${currentStep === 5 ? 'active' : ''}`}>Membros</span>
+                        <span className={`step ${currentStep === 6 ? 'active' : ''}`}>Confirmação</span>
+                    </header>
+                    <div id="line-container">
+                        <div 
+                            id="line" 
+                            style={{ transform: `translateX(${(currentStep - 1) * 100}%)` }}    
+                        />
+                    </div>
+                </main>
+            </div>
         </div>
     )
 }
